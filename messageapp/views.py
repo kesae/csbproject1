4fe5d79553afe9ebc71cf3ca292820ae1a8618d3
kesae <keikkosa@outlook.com>
2024-05-20 13:49:19 +0300

    Set up URLs

--- messageapp/views.py
+++ messageapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Main page")
+
+def search(request):
+    if request.method == "GET":
+        query = request.GET.get('q','')
+    return HttpResponse("Search page")
+
+def user(request, uid):
+    return HttpResponse("User page")
